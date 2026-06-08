c51ef2d763a6ccf0855df7b69ada6bf01938d657
kenyan page <kpage48@wgu.edu>
2026-06-08 12:46:17 -0500

    Installed Breeze and Alpine Blade

---
+++ database/migrations/2026_06_01_141747_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->boolean('countable')->default(false);
+            $table->unsignedInteger('quantity')->nullable();
+            $table->unsignedInteger('original_quantity')->nullable();
+            $table->decimal('price', 8, 2)->nullable();
+            $table->date('expiration_date')->nullable();
+            $table->boolean('refrigerated')->default(false);
+            $table->foreignId('pantry_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('image')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
